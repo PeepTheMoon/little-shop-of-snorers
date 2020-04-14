c6ef6fba69661d12164df7c185e215985d305976
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-04-14 11:21:26 -0700

    chart added, will write functions to fill in data later

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
+// Import original array and aray copy .slice()
+
 import { productData } from './product-data.js';
+import { ProductsArray } from './products-array.js';
+
--- products-array.js
+++ products-array.js
@@ -1,9 +1,15 @@
+//
+
 export class ProductsArray {
+    //constructor that intializes an object within a class
     constructor(products) {
+        // .slice() to make a copy without altering the original array
         this.products = products.slice();
+       // return the copy
     }
     getProducts() {
         return this.products;
     }
+    // get product images
     
 }
